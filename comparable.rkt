074414ea5b999720cf8c718f38e5563b4bf6c84d
Siddhartha <sid@countvajhula.com>
2019-11-19 12:01:24 -0800

    initial commit - simple generic interface for order relations

---
+++ comparable.rkt
@@ -0,0 +1,43 @@
+#lang racket
+
+(provide gen:comparable
+         comparable?
+         comparable/c
+         <
+         <=
+         =
+         >=
+         >)
+
+(require (prefix-in b: racket/base)
+         racket/generic)
+
+;; define a generic `comparable` interface
+(define-generics comparable
+  (< comparable other)
+  (<= comparable other)
+  (= comparable other)
+  (>= comparable other)
+  (> comparable other)
+  #:defaults ([number?
+               (define (< comparable other)
+                 (b:< comparable other))
+               (define (<= comparable other)
+                 (b:<= comparable other))
+               (define (= comparable other)
+                 (b:= comparable other))
+               (define (>= comparable other)
+                 (b:>= comparable other))
+               (define (> comparable other)
+                 (b:> comparable other))]
+              [string?
+               (define (< comparable other)
+                 (string<? comparable other))
+               (define (<= comparable other)
+                 (string<=? comparable other))
+               (define (= comparable other)
+                 (string=? comparable other))
+               (define (>= comparable other)
+                 (string>=? comparable other))
+               (define (> comparable other)
+                 (string>? comparable other))]))
