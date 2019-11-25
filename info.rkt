a265aeaf01fd7541b159d5c8979f1fc8eb29a10e
Siddhartha <sid@countvajhula.com>
2019-11-25 10:33:10 -0800

    add missing dependency

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "relation")
-(define deps '("base"))
+(define deps '("base" "collections-lib"))
 (define build-deps '("scribble-lib" "scribble-abbrevs" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/relation.scrbl" (multi-page))))
 (define pkg-desc "Generic interfaces and convenient utilities for relations")
