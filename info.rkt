188d5cb3c94e4d254e584cedc82ef62cd5790be2
Siddhartha <sid@countvajhula.com>
2019-11-26 00:54:40 -0800

    bump version -> 0.2

--- info.rkt
+++ info.rkt
@@ -4,5 +4,5 @@
 (define build-deps '("scribble-lib" "scribble-abbrevs" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/relation.scrbl" (multi-page))))
 (define pkg-desc "Generic interfaces and convenient utilities for relations")
-(define version "0.0")
+(define version "0.2")
 (define pkg-authors '(countvajhula))
