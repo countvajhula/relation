31c0db5931f3f16bf75070a995758a70a54f0e51
Siddhartha <sid@countvajhula.com>
2019-11-27 17:22:20 -0800

    add Makefile

--- info.rkt
+++ info.rkt
@@ -8,6 +8,7 @@
                      "rackunit-lib"
                      "sandbox-lib"))
 (define scribblings '(("scribblings/relation.scrbl" (multi-page))))
+(define clean '("compiled" "doc" "doc/relation"))
 (define pkg-desc "Generic interfaces and convenient utilities for relations")
 (define version "0.2")
 (define pkg-authors '(countvajhula))
