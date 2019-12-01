1274ef52a84b4001a5d8491020923597c98f1555
Siddhartha <sid@countvajhula.com>
2019-11-30 21:10:18 -0800

    benchmark a few more interfaces in the transform module

--- dev/profile/transform/builtin.rkt
+++ dev/profile/transform/builtin.rkt
@@ -12,4 +12,19 @@
   (for ([i (take how-many (in (cycle '("1" "2" "3"))))])
     (string->number i)))
 
+(define (check-number-to-string how-many)
+  (for ([i (take how-many (in (cycle '(1 2 3))))])
+    (number->string i)))
+
+(define (check-list-to-vector how-many)
+  (for ([i (take how-many (in (cycle '((1 2 3) (4 5 6) (7 8 9)))))])
+    (list->vector i)))
+
+(define (check-vector-to-list how-many)
+  (for ([i (take how-many (in (cycle '(#(1 2 3) #(4 5 6) #(7 8 9)))))])
+    (vector->list i)))
+
 (check-string-to-number 10000)
+(check-number-to-string 10000)
+(check-list-to-vector 10000)
+(check-vector-to-list 10000)
