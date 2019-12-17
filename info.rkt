a061007bbf420778681d5d0f3c967946fb16459d
Siddhartha <sid@countvajhula.com>
2019-12-17 11:53:19 -0800

    bump version -> 1.0

--- info.rkt
+++ info.rkt
@@ -10,5 +10,5 @@
 (define scribblings '(("scribblings/relation.scrbl" (multi-page))))
 (define clean '("compiled" "doc" "doc/relation"))
 (define pkg-desc "Generic interfaces and convenient utilities for relations")
-(define version "0.7")
+(define version "1.0")
 (define pkg-authors '(countvajhula))
