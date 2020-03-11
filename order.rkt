6ef2b6a09cd9524700b456e562ea3feecc672cf0
Siddhartha <sid@countvajhula.com>
2020-03-11 12:23:36 -0700

    simplify order interface to pairwise checks
        - this also brings performance back to pre-1f36bdb levels
    	  unclear why

--- order.rkt
+++ order.rkt
@@ -4,6 +4,10 @@
          racket/set
          racket/contract/base
          racket/generic
+         (only-in algebraic/prelude
+                  flip
+                  ||
+                  &&)
          racket/function
          racket/stream
          (only-in racket/list
@@ -60,103 +64,59 @@
                     orderable?))))
 
 (define-generics orderable
-  (less-than? orderable . others)
-  (less-than-or-equal? orderable . others)
-  (greater-than-or-equal? orderable . others)
-  (greater-than? orderable . others)
+  (less-than? orderable other)
+  (less-than-or-equal? orderable other)
+  (greater-than-or-equal? orderable other)
+  (greater-than? orderable other)
   #:fast-defaults ([number?
-                    (define (less-than? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise b:< vals)))
-                    (define (less-than-or-equal? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise b:<= vals)))
-                    (define (greater-than-or-equal? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise b:>= vals)))
-                    (define (greater-than? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise b:> vals)))]
+                    (define less-than? b:<)
+                    (define less-than-or-equal? b:<=)
+                    (define greater-than-or-equal? b:>=)
+                    (define greater-than? b:>)]
                    [string?
-                    (define (less-than? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise string<? vals)))
-                    (define (less-than-or-equal? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise string<=? vals)))
-                    (define (greater-than-or-equal? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise string>=? vals)))
-                    (define (greater-than? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise string>? vals)))]
+                    (define less-than? string<?)
+                    (define less-than-or-equal? string<=?)
+                    (define greater-than-or-equal? string>=?)
+                    (define greater-than? string>?)]
                    [bytes?
-                    (define (less-than? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise bytes<? vals)))
-                    (define (less-than-or-equal? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise (λ (a b)
-                                          (or (bytes=? a b)
-                                              (bytes<? a b)))
-                                        vals)))
-                    (define (greater-than-or-equal? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise (λ (a b)
-                                          (or (bytes=? a b)
-                                              (bytes>? a b)))
-                                        vals)))
-                    (define (greater-than? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise bytes>? vals)))]
+                    (define less-than? bytes<?)
+                    (define less-than-or-equal?
+                      (|| bytes=?
+                          bytes<?))
+                    (define greater-than-or-equal?
+                      (|| bytes=?
+                          bytes>?))
+                    (define greater-than? bytes>?)]
                    [char?
-                    (define (less-than? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise char<? vals)))
-                    (define (less-than-or-equal? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise char<=? vals)))
-                    (define (greater-than-or-equal? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise char>=? vals)))
-                    (define (greater-than? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise char>? vals)))]
+                    (define less-than? char<?)
+                    (define less-than-or-equal? char<=?)
+                    (define greater-than-or-equal? char>=?)
+                    (define greater-than? char>?)]
                    [set?
-                    (define (less-than? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise proper-subset? vals)))
-                    (define (less-than-or-equal? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise subset? vals)))
-                    (define (greater-than-or-equal? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise subset?
-                                        (reverse vals))))
-                    (define (greater-than? orderable . others)
-                      (let ([vals (cons orderable others)])
-                        (check-pairwise proper-subset?
-                                        (reverse vals))))]))
+                    (define less-than? proper-subset?)
+                    (define less-than-or-equal? subset?)
+                    (define greater-than-or-equal? (flip subset?))
+                    (define greater-than? (flip proper-subset?))]))
 
 (define (< #:key [key #f] . args)
   (if key
       (apply < (map key args))
-      (apply less-than? args)))
+      (check-pairwise less-than? args)))
 
 (define (<= #:key [key #f] . args)
   (if key
       (apply <= (map key args))
-      (apply less-than-or-equal? args)))
+      (check-pairwise less-than-or-equal? args)))
 
 (define (>= #:key [key #f] . args)
   (if key
       (apply >= (map key args))
-      (apply greater-than-or-equal? args)))
+      (check-pairwise greater-than-or-equal? args)))
 
 (define (> #:key [key #f] . args)
   (if key
       (apply > (map key args))
-      (apply greater-than? args)))
+      (check-pairwise greater-than? args)))
 
 (define (min #:key [key #f] . args)
   (first (sort args
