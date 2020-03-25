345bd161489faddb5ba5d708d9de609ee4609455
Siddhartha <sid@countvajhula.com>
2020-03-25 13:43:47 -0700

    bump version 1.5 -> 1.5.1

--- info.rkt
+++ info.rkt
@@ -15,5 +15,5 @@
 (define test-include-paths '("tests"))
 (define clean '("compiled" "doc" "doc/relation" "tests/compiled"))
 (define pkg-desc "Generic interfaces and convenient utilities for relations")
-(define version "1.5")
+(define version "1.5.1")
 (define pkg-authors '(countvajhula))
