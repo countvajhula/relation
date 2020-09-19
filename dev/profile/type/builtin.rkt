cd2eb5ad2cb260065519ee644e8aac45b7c46279
Siddhartha <sid@countvajhula.com>
2020-09-18 17:14:56 -0700

    smoke profilers for generic constructor

--- dev/profile/type/builtin.rkt
+++ dev/profile/type/builtin.rkt
@@ -2,6 +2,7 @@
 
 (require (prefix-in b: racket/base)
          racket/stream
+         racket/vector
          data/collection)
 
 (define (randoms n)
@@ -24,7 +25,35 @@
   (for ([i (take how-many (in (cycle '(#(1 2 3) #(4 5 6) #(7 8 9)))))])
     (vector->list i)))
 
+(define (check-make-pairs how-many)
+  (for ([i (take how-many (in (cycle '((a . 1) (b . 2) (c . 3)))))])
+    (cons (car i) (cdr i))
+    (cons (cdr i) (car i))))
+
+(define (check-make-lists how-many)
+  (for ([j (take how-many (in (cycle '(1 2 3))))])
+    (cons j (list 5 6 7))
+    (cons j (list 6 7 8))
+    (cons j (list 7 8 9))))
+
+(define (check-make-vectors how-many)
+  (for ([j (take how-many (in (cycle '(1 2 3))))])
+    (vector->immutable-vector
+     (vector-append #(5 6 7) (vector j)))
+    (vector->immutable-vector
+     (vector-append #(6 7 8) (vector j)))
+    (vector->immutable-vector
+     (vector-append #(7 8 9) (vector j)))))
+
+(define (check-make-hashes how-many)
+  (for ([j (take how-many (in (cycle '((a . 1) (b . 2) (c . 3)))))])
+    (hash-set (hash 'd 4) (car j) (cdr j))))
+
 (check-string-to-number 10000)
 (check-number-to-string 10000)
 (check-list-to-vector 10000)
 (check-vector-to-list 10000)
+(check-make-pairs 10000)
+(check-make-lists 10000)
+(check-make-vectors 10000)
+(check-make-hashes 10000)
--- dev/profile/type/relation.rkt
+++ dev/profile/type/relation.rkt
@@ -28,7 +28,32 @@
   (for ([i (take how-many (in (cycle '(#(1 2 3) #(4 5 6) #(7 8 9)))))])
     (->list i)))
 
+(define (check-make-pairs how-many)
+  (for ([i (take how-many (in (cycle '((a . 1) (b . 2) (c . 3)))))])
+    (: (car i) (cdr i))
+    (: (cdr i) (car i))))
+
+(define (check-make-lists how-many)
+  (for ([j (take how-many (in (cycle '(1 2 3))))])
+    (: j (list 5 6 7))
+    (: j (list 6 7 8))
+    (: j (list 7 8 9))))
+
+(define (check-make-vectors how-many)
+  (for ([j (take how-many (in (cycle '(1 2 3))))])
+    (: j #(5 6 7))
+    (: j #(6 7 8))
+    (: j #(7 8 9))))
+
+(define (check-make-hashes how-many)
+  (for ([j (take how-many (in (cycle '((a . 1) (b . 2) (c . 3)))))])
+    (: j (hash 'd 4))))
+
 (check-string-to-number 10000)
 (check-number-to-string 10000)
 (check-list-to-vector 10000)
 (check-vector-to-list 10000)
+(check-make-pairs 10000)
+(check-make-lists 10000)
+(check-make-vectors 10000)
+(check-make-hashes 10000)
