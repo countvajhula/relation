9f16852a3478fee03f06d766436845553f2789a5
Siddhartha <sid@countvajhula.com>
2021-06-04 13:35:08 -0700

    remove old unused main submodule

--- main.rkt
+++ main.rkt
@@ -1,7 +1,5 @@
 #lang racket/base
 
-;; Code here
-
 (require "logic.rkt"
          "equivalence.rkt"
          "order.rkt"
@@ -15,18 +13,3 @@
                        "function.rkt"
                        "type.rkt"
                        "composition.rkt"))
-
-(module+ main
-  ;; (Optional) main submodule. Put code here if you need it to be executed when
-  ;; this file is run using DrRacket or the `racket` executable.  The code here
-  ;; does not run when this file is required by another module. Documentation:
-  ;; http://docs.racket-lang.org/guide/Module_Syntax.html#%28part._main-and-test%29
-
-  (require racket/cmdline)
-  (define who (box "world"))
-  (command-line
-    #:program "my-program"
-    #:once-each
-    [("-n" "--name") name "Who to say hello to" (set-box! who name)]
-    #:args ()
-    (printf "hello ~a~n" (unbox who))))
