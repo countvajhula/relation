416cee369235b20729ce186a90259b2aef436807
Siddhartha <sid@countvajhula.com>
2021-06-11 21:42:20 -0700

    restore an application-scheme liaison module

---
+++ function/types/application-scheme.rkt
@@ -0,0 +1,10 @@
+#lang racket/base
+
+(require relation/function/types/application-scheme/interface
+         relation/function/types/application-scheme/curried
+         relation/function/types/application-scheme/template)
+
+(provide (all-from-out
+          relation/function/types/application-scheme/interface
+          relation/function/types/application-scheme/curried
+          relation/function/types/application-scheme/template))
