da79c914787bd5b4a4449315711ed86672be1111
Siddhartha <sid@countvajhula.com>
2021-06-18 19:55:57 -0700

    infer arity better; and add tests for it

--- function/type/application-scheme/private/util.rkt
+++ function/type/application-scheme/private/util.rkt
@@ -1,11 +1,20 @@
 #lang racket/base
 
-(require ionic)
+(require ionic
+         (only-in racket/function curry)
+         (rename-in racket/function
+                    [negate !!]
+                    [conjoin &&]))
 
-(require "../../interface.rkt")
+(require "../../interface.rkt"
+         (only-in "../../../../private/util.rkt"
+                  singleton?))
 
 (provide min-arity
          revise-arity)
+
+(module+ test
+  (require rackunit))
 
 (define-switch (~min-arity-value arity)
   [number? arity]
@@ -21,21 +30,35 @@
 (define (revise-arity v n)
   (let loop ([v v])
     (switch (v)
-            ;; in the case of a curried case-lambdas, it may be
-            ;; that we've received m arguments and the case arities
-            ;; are a and b where a < m < b. In this case we know
-            ;; we are in case b, so arguably we should simply
-            ;; exclude this case from the list, flatting the list
-            ;; in case of a single arity value. But for now,
-            ;; we simply ensure it doesn't go below 0 since that's
-            ;; an invalid arity value, since we don't immediately
-            ;; know if the value is being used inside a list
-            [number? (call (~> (- n) (max 0)))]
+            [number? (call (- n))]
             [arity-at-least? (call (~> arity-at-least-value
                                        (- n)
                                        (max 0)
                                        arity-at-least))]
-            [list? (call (~>> (map loop)))]
+            ;; in the case of a curried case-lambda, it may be
+            ;; that we've received m arguments and the case arities
+            ;; are a and b where a < m < b. In this case we know
+            ;; we are in case b, so we simply exclude the other
+            ;; case from the list, flatting the list in case we
+            ;; are left with a single arity value.
+            [list? (call (~>> (map loop)
+                              (esc (curry filter
+                                          (!! (&& integer?
+                                                  negative?))))
+                              (if singleton? car _)))]
             [else (call (raise-argument-error 'revise-arity
                                               "normalized-arity?"
                                               _))])))
+
+(module+ test
+  (test-suite
+      "revise-arity"
+    (check-equal? (revise-arity 5 3) 2)
+    (check-equal? (revise-arity 2 3) -1 "an invalid arity, but not the responsibility of this function to check")
+    (check-equal? (revise-arity (arity-at-least 3) 2) (arity-at-least 1))
+    (check-equal? (revise-arity (arity-at-least 3) 3) (arity-at-least 0))
+    (check-equal? (revise-arity (arity-at-least 3) 4) (arity-at-least 0))
+    (check-equal? (revise-arity (list 3 4) 2) (list 1 2))
+    (check-equal? (revise-arity (list (arity-at-least 3) 4) 2) (list (arity-at-least 1) 2))
+    (check-equal? (revise-arity (list (arity-at-least 3) 4) 4) (list (arity-at-least 0) 0))
+    (check-equal? (revise-arity (list 3 5) 4) 1)))
