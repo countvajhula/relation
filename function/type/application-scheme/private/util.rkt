4d9b5a57249d12294e1de4469c113ae2c28bba40
Siddhartha <sid@countvajhula.com>
2021-07-01 20:38:25 -0700

    actually run tests in submodule

--- function/type/application-scheme/private/util.rkt
+++ function/type/application-scheme/private/util.rkt
@@ -16,7 +16,9 @@
          list-subtract)
 
 (module+ test
-  (require rackunit))
+  (require rackunit
+           rackunit/text-ui
+           "../../../../tests/private/util.rkt"))
 
 (define-switch (~min-arity-value arity)
   [number? arity]
@@ -63,23 +65,31 @@
                 (cons v (list-subtract vs lst2)))])]))
 
 (module+ test
-  (test-suite
-      "revise-arity"
-    (check-equal? (revise-arity 5 3) 2)
-    (check-equal? (revise-arity 2 3) -1 "an invalid arity, but not the responsibility of this function to check")
-    (check-equal? (revise-arity (arity-at-least 3) 2) (arity-at-least 1))
-    (check-equal? (revise-arity (arity-at-least 3) 3) (arity-at-least 0))
-    (check-equal? (revise-arity (arity-at-least 3) 4) (arity-at-least 0))
-    (check-equal? (revise-arity (list 3 4) 2) (list 1 2))
-    (check-equal? (revise-arity (list (arity-at-least 3) 4) 2) (list (arity-at-least 1) 2))
-    (check-equal? (revise-arity (list (arity-at-least 3) 4) 4) (list (arity-at-least 0) 0))
-    (check-equal? (revise-arity (list 3 5) 4) 1))
 
-  (test-suite
-   "list-subtract"
-   (check-equal? (list-subtract (list 1 2 3) (list 2)) (list 1 3))
-   (check-equal? (list-subtract (list 1 2 2 3) (list 2)) (list 1 3) "removes all instances if found")
-   (check-equal? (list-subtract (list 1 2 3) (list 5)) (list 1 2 3))
-   (check-equal? (list-subtract (list 1 2 3) null) (list 1 2 3))
-   (check-equal? (list-subtract null (list 1 2 3)) null)
-   (check-equal? (list-subtract null null) null)))
+  (define tests
+    (test-suite
+     "application scheme utilities"
+
+     (test-suite
+      "revise-arity"
+      (check-equal? (revise-arity 5 3) 2)
+      (check-equal? (revise-arity 2 3) -1 "an invalid arity, but not the responsibility of this function to check")
+      (check-equal? (revise-arity (arity-at-least 3) 2) (arity-at-least 1))
+      (check-equal? (revise-arity (arity-at-least 3) 3) (arity-at-least 0))
+      (check-equal? (revise-arity (arity-at-least 3) 4) (arity-at-least 0))
+      (check-equal? (revise-arity (list 3 4) 2) (list 1 2))
+      (check-equal? (revise-arity (list (arity-at-least 3) 4) 2) (list (arity-at-least 1) 2))
+      (check-equal? (revise-arity (list (arity-at-least 3) 4) 4) (list (arity-at-least 0) 0))
+      (check-equal? (revise-arity (list 3 5) 4) 1))
+
+     (test-suite
+      "list-subtract"
+      (check-equal? (list-subtract (list 1 2 3) (list 2)) (list 1 3))
+      (check-equal? (list-subtract (list 1 2 2 3) (list 2)) (list 1 3) "removes all instances if found")
+      (check-equal? (list-subtract (list 1 2 3) (list 5)) (list 1 2 3))
+      (check-equal? (list-subtract (list 1 2 3) null) (list 1 2 3))
+      (check-equal? (list-subtract null (list 1 2 3)) null)
+      (check-equal? (list-subtract null null) null))))
+
+  (just-do
+   (run-tests tests)))
