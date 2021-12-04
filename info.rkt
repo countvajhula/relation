eb1c10cf3a4704c369183d770abb1ab50ef37a70
Siddhartha <sid@countvajhula.com>
2021-12-03 19:33:22 -0800

    revert dependency change

--- info.rkt
+++ info.rkt
@@ -9,7 +9,7 @@
                "threading-lib"
                "mischief"
                "social-contract"
-               "qi-lib"
+               "qi"
                "kw-utils"
                "typed-stack"
                "version-case"
