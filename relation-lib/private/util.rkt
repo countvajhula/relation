e66d512c4509a70fe09b5f4df511b32f60609040
Siddhartha <sid@countvajhula.com>
2022-09-22 08:20:11 -0700

    use version-case for define-syntax-parse-rule

--- relation-lib/private/util.rkt
+++ relation-lib/private/util.rkt
@@ -4,6 +4,7 @@
          racket/match
          arguments
          syntax/parse/define
+         version-case
          qi
          (for-syntax racket/base))
 
@@ -16,6 +17,11 @@
          join-list
          singleton?
          arguments-cons)
+
+(version-case
+ [(version< (version) "7.9.0.22")
+  (define-syntax define-syntax-parse-rule
+    (make-rename-transformer #'define-simple-macro))])
 
 (define-syntax-parse-rule (define-alias alias:id name:id)
   (define-syntax alias (make-rename-transformer #'name)))
